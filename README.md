546d3da063b73cd54fce0de53b7656643eb1439c
Ferlony <hegecioum@gmail.com>
2025-01-21 07:21:46 +0400

    feat: update doom emacs conf

--- README.md
+++ README.md
@@ -7,10 +7,15 @@ sudo pacman -S ripgrep fd shellcheck && \
 sudo npm install -g marked
 ```
 
+To enable python pyright lsp server need to install
+
+``` shell
+sudo npm i -g pyright
+```
+
+
 To enable word-wrap to new line without changing buffer
 ``` emacs-lisp
 
 global-visual-line-mode 
 ```
-
-
--- doom/custom.el
+++ doom/custom.el
@@ -3,8 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(copilot-chat-model "gpt-4o")
  '(package-selected-packages
-   '(copilot-chat copilot treemacs zig-mode markdown-preview-eww rainbow-mode rgb)))
+   '(lazy-ruff python-black lsp-pyright copilot-chat copilot treemacs zig-mode markdown-preview-eww rainbow-mode rgb)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -24,3 +25,15 @@
 (add-hook! 'rainbow-mode-hook
   (hl-line-mode (if rainbow-mode -1 +1)))
 
+;; accept completion from copilot and fallback to company
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
+
+
+;; init packages at start up
+(package-initialize)
