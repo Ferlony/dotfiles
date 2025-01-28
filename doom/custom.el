4e631a538714d2d4d5023311695efb0569afc9cd
Ferlony <hegecioum@gmail.com>
2025-01-29 02:21:51 +0500

    feat: Update doom conf

--- doom/custom.el
+++ doom/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(copilot-chat-model "gpt-4o")
  '(package-selected-packages
-   '(lazy-ruff python-black lsp-pyright copilot-chat copilot treemacs zig-mode markdown-preview-eww rainbow-mode rgb)))
+   '(go-mode lsp-jedi jedi elpy protobuf-mode clang-format solidity-flycheck solidity-mode pipenv poetry python-environment pyenv-mode lazy-ruff python-black lsp-pyright copilot-chat copilot treemacs zig-mode markdown-preview-eww rainbow-mode rgb)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -29,6 +29,12 @@
 (add-hook! 'rainbow-mode-hook
   (hl-line-mode (if rainbow-mode -1 +1)))
 
+(add-hook 'python-mode-hook 'lsp)
+(add-hook 'cpp-mode-hook 'lsp)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'cs-mode-hook 'lsp)
+
+
 ;; accept completion from copilot and fallback to company
 (use-package! copilot
   :hook (prog-mode . copilot-mode)
@@ -37,7 +43,6 @@
               ("TAB" . 'copilot-accept-completion)
               ("C-TAB" . 'copilot-accept-completion-by-word)
               ("C-<tab>" . 'copilot-accept-completion-by-word)))
-
 
 ;; init packages at start up
 (package-initialize)
