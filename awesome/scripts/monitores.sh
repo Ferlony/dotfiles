730f554a55ebf536a42a6d04078d47916f147e55
Ferlony <hegecioum@gmail.com>
2025-02-03 11:22:01 +0500

    fix: Add brightness to awesome monitor script

--- awesome/scripts/monitores.sh
+++ awesome/scripts/monitores.sh
@@ -5,7 +5,6 @@ extern=HDMI2
 if xrandr | grep "$extern disconnected"; then
     xrandr --output "$extern" --off --output "$intern" --auto --scale 1x1 --mode 1920x1080
 else
-    # xrandr --output "$intern" --off --output "$extern" --auto --scale 1x1 --mode 1920x1080
-    xrandr --output $intern --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output $extern --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+    xrandr --output $intern --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output $extern --mode 1920x1080 --brightness 1.0 --pos 0x0 --rotate normal --output VIRTUAL1 --off 
 
 fi
